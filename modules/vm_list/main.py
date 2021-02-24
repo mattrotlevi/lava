23c50c3d6660bd04d6c288aae095ba1908d62deb
David Okeyode <asegunlolu@hotmail.com>
2021-02-24 00:41:03 +0000

    added managed identity and role to vm_list module

--- modules/vm_list/main.py
+++ modules/vm_list/main.py
@@ -16,14 +16,17 @@ def start(args):
 
 		print("[+] getting vm's in subscription [+]")
 		
-		vm_list = subprocess.run(['az','vm', 'list', '--query', '[].{name:name,os:storageProfile.osDisk.osType, username:osProfile.adminUsername, vm_size:hardwareProfile.vmSize, resource_group: resourceGroup}'], stdout=subprocess.PIPE)
+		vm_list = subprocess.run(['az','vm', 'list', '--query', '[].{name:name,os:storageProfile.osDisk.osType, username:osProfile.adminUsername, vm_size:hardwareProfile.vmSize, resource_group:resourceGroup, managed_identity:identity.type, app_id:identity.principalId}'], stdout=subprocess.PIPE)
 		vm_list_json = json.loads(vm_list.stdout.decode('utf-8'))
 
 		vm_iplist = subprocess.run(['az','vm', 'list-ip-addresses', '--query', '[].{name:virtualMachine.name, privateIp:virtualMachine.network.privateIpAddresses, publicIp:virtualMachine.network.publicIpAddresses[].ipAddress}'], stdout=subprocess.PIPE)
 		vm_iplist_json = json.loads(vm_iplist.stdout.decode('utf-8'))
 
 		for i in range(len(vm_list_json)):
+			vm_identity_role = subprocess.run(['az','role', 'assignment', 'list', '--assignee', vm_list_json[i]['app_id'], '--include-groups', '--include-inherited', '--query', '[].{role:roleDefinitionName, scope:scope}'], stdout=subprocess.PIPE)
+			vm_identity_role_json = json.loads(vm_identity_role.stdout.decode('utf-8'))
 			vm_list_json[i].update(vm_iplist_json[i])
+			vm_list_json[i].update(vm_identity_role_json[i])
 		
 		pprint.pprint(vm_list_json)
 
@@ -36,14 +39,17 @@ def start(args):
 		ind = args[0].index("-rgrp")
 		resource_grp_name = args[0][ind+1]
 
-		vm_list = subprocess.run(['az','vm', 'list', '-g', resource_grp_name, '--query', '[].{name:name,os:storageProfile.osDisk.osType, username:osProfile.adminUsername, vm_size:hardwareProfile.vmSize, resource_group: resourceGroup}'], stdout=subprocess.PIPE)
+		vm_list = subprocess.run(['az','vm', 'list', '-g', resource_grp_name, '--query', '[].{name:name,os:storageProfile.osDisk.osType, username:osProfile.adminUsername, vm_size:hardwareProfile.vmSize, resource_group:resourceGroup, managed_identity:identity.type, app_id:identity.principalId}'], stdout=subprocess.PIPE)
 		vm_list_json = json.loads(vm_list.stdout.decode('utf-8'))
 
 		vm_iplist = subprocess.run(['az','vm', 'list-ip-addresses', '-g', resource_grp_name, '--query', '[].{name:virtualMachine.name, privateIp:virtualMachine.network.privateIpAddresses, publicIp:virtualMachine.network.publicIpAddresses[].ipAddress}'], stdout=subprocess.PIPE)
 		vm_iplist_json = json.loads(vm_iplist.stdout.decode('utf-8'))
 
 		for i in range(len(vm_list_json)):
+			vm_identity_role = subprocess.run(['az','role', 'assignment', 'list', '--assignee', vm_list_json[i]['app_id'], '--include-groups', '--include-inherited', '--query', '[].{role:roleDefinitionName, scope:scope}'], stdout=subprocess.PIPE)
+			vm_identity_role_json = json.loads(vm_identity_role.stdout.decode('utf-8'))
 			vm_list_json[i].update(vm_iplist_json[i])
+			vm_list_json[i].update(vm_identity_role_json[i])
 		
 		pprint.pprint(vm_list_json)
 
