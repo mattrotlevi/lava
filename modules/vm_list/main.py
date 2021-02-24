325c9902fb6ce43e8e7b35848fbde6234c845348
David Okeyode <asegunlolu@hotmail.com>
2021-02-24 01:38:14 +0000

    modified vm_list module

--- modules/vm_list/main.py
+++ modules/vm_list/main.py
@@ -21,14 +21,15 @@ def start(args):
 
 		vm_iplist = subprocess.run(['az','vm', 'list-ip-addresses', '--query', '[].{name:virtualMachine.name, privateIp:virtualMachine.network.privateIpAddresses, publicIp:virtualMachine.network.publicIpAddresses[].ipAddress}'], stdout=subprocess.PIPE)
 		vm_iplist_json = json.loads(vm_iplist.stdout.decode('utf-8'))
-
+		
 		for i in range(len(vm_list_json)):
+			if vm_list_json[i]['app_id'] != "null":
 			vm_identity_role = subprocess.run(['az','role', 'assignment', 'list', '--assignee', vm_list_json[i]['app_id'], '--include-groups', '--include-inherited', '--query', '[].{role:roleDefinitionName, scope:scope}'], stdout=subprocess.PIPE)
 			vm_identity_role_json = json.loads(vm_identity_role.stdout.decode('utf-8'))
 			vm_list_json[i].update(vm_iplist_json[i])
 			vm_list_json[i].update(vm_identity_role_json[i])
 		
-		pprint.pprint(vm_list_json)
+		pprint.pprint(vm_list_json)	
 
 		return
 	
@@ -46,6 +47,10 @@ def start(args):
 		vm_iplist_json = json.loads(vm_iplist.stdout.decode('utf-8'))
 
 		for i in range(len(vm_list_json)):
+			vm_list_json[i].update(vm_iplist_json[i])
+		
+		for i in range(len(vm_list_json)):
+			if vm_list_json[i]['app_id'] != "null":
 			vm_identity_role = subprocess.run(['az','role', 'assignment', 'list', '--assignee', vm_list_json[i]['app_id'], '--include-groups', '--include-inherited', '--query', '[].{role:roleDefinitionName, scope:scope}'], stdout=subprocess.PIPE)
 			vm_identity_role_json = json.loads(vm_identity_role.stdout.decode('utf-8'))
 			vm_list_json[i].update(vm_iplist_json[i])
