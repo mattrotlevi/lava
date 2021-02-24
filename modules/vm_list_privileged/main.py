7a148408275003185cb0c827e43eba8cb3ccd412
David Okeyode <asegunlolu@hotmail.com>
2021-02-24 23:26:24 +0000

    fixed error in vm_list_privileged module

--- modules/vm_list_privileged/main.py
+++ modules/vm_list_privileged/main.py
@@ -33,32 +33,3 @@ def start(args):
                     else:
                         print("[+++++++] vm is not privileged [+++++++]")
                         print("[++++++++++++++++++++++++++++++++++++++]")
-
-            #return
-
-        if "-rgrp" in args[0]:
-
-                print("[+] getting vm's in specified resource group [+]")
-
-                ind = args[0].index("-rgrp")
-                resource_grp_name = args[0][ind+1]
-
-                vm_list = subprocess.run(['az','vm', 'list', '-g', resource_grp_name, '--query', '[].{name:name,os:storageProfile.osDisk.osType, username:osProfile.adminUsername, vm_size:hardwareProfile.vmSize, resource_group:resourceGroup, managed_identity:identity.type, app_id:identity.principalId}'], stdout=subprocess.PIPE)
-                vm_list_json = json.loads(vm_list.stdout.decode('utf-8'))
-
-                vm_iplist = subprocess.run(['az','vm', 'list-ip-addresses', '-g', resource_grp_name, '--query', '[].{name:virtualMachine.name, privateIp:virtualMachine.network.privateIpAddresses, publicIp:virtualMachine.network.publicIpAddresses[].ipAddress}'], stdout=subprocess.PIPE)
-                vm_iplist_json = json.loads(vm_iplist.stdout.decode('utf-8'))
-
-                for i in range(len(vm_list_json)):
-                    vm_list_json[i].update(vm_iplist_json[i])
-                    if vm_list_json[i]['app_id'] != None:
-                        vm_identity_role = subprocess.run(['az','role', 'assignment', 'list', '--assignee', vm_list_json[i]['app_id'], '--include-groups', '--include-inherited', '--query', '[].{role:roleDefinitionName, scope:scope}'], stdout=subprocess.PIPE)
-                        vm_identity_role_json = json.loads(vm_identity_role.stdout.decode('utf-8'))
-                        pprint.pprint(vm_list_json[i])
-                        pprint.pprint(vm_identity_role_json)
-                        print("[++++++++++++++++++++++++++++++++++++++]")
-                    else:
-                        print("[+++++++] vm found but not privileged [+++++++]")
-                        print("[++++++++++++++++++++++++++++++++++++++]")
-
-                    pprint.pprint(vm_list_json)
